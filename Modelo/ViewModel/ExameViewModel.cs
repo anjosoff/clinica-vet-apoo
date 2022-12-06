c13979df22693a744ae4f09f3da8dded761041d6
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-12-05 23:48:28 -0300

    FEAT: Add ViewModel

---
+++ Modelo/ViewModel/ExameViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modelo.ViewModels
+{
+    public class ExameViewModel
+    {
+       public int Id { get; set; }
+       public string Descricao { get; set; }
+       public virtual ICollection<ConsultaViewModel> Consultas { get; set; }
+    }
+}
