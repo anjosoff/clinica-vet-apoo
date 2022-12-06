de2b936bdb6d13bd42291856a4e155f53bdbd108
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-12-06 01:54:39 -0300

    FEAT: Add migrations

---
+++ Persistenciaa/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace Persistencia.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Persistencia.Contexts.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Persistencia.Contexts.EFContext context)
+        {
+           
+        }
+    }
+}
