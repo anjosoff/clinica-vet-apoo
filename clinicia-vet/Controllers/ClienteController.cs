064cfe221bd4d97042357f96de97bd80b03ceaec
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-12-05 23:49:04 -0300

    FEAT: Controladores

---
+++ clinicia-vet/Controllers/ClienteController.cs
@@ -0,0 +1,103 @@
+﻿using System.Web.Mvc;
+using Servico;
+using Modelo;
+using System.Net;
+using Modelo;
+using Persistencia.Contexts;
+using Servico;
+
+namespace clinicia_vet.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private ClienteServico clienteServico = new ClienteServico();
+        private ActionResult ObterVisaoClientePorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = clienteServico.ObterClientePorId((long)id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        private ActionResult GravarCliente(Cliente cliente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    clienteServico.GravarCliente(cliente);
+                    return RedirectToAction("Index");
+                }
+                return View(cliente);
+            }
+            catch
+            {
+                return View(cliente);
+            }
+        }
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(clienteServico.ObterClientesClassificadosPorNome());
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente cliente)
+        {
+            return GravarCliente(cliente);
+        }
+        // GET: Edit
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoClientePorId(id);
+        }
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Cliente cliente)
+        {
+            return GravarCliente(cliente);
+        }
+        //GET: Details
+        public ActionResult Details(int id)
+        {
+            return ObterVisaoClientePorId(id);
+        }
+
+        // GET: Delete
+        public ActionResult Delete(int? id)
+        {
+            return ObterVisaoClientePorId(id);
+        }
+        // POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Cliente cliente = clienteServico.EliminarClientePorId(id);
+                TempData["Message"] = "Cliente " + cliente.Nome.ToUpper() + " foi removido";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ clinicia-vet/Controllers/ExameController.cs
@@ -0,0 +1,101 @@
+﻿using Modelo;
+using Persistencia.Contexts;
+using clinica_vet.Servico;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clinicia_vet.Controllers
+{
+    public class ExameController : Controller
+    {
+        private ExameServico exameServico = new ExameServico();
+        private ActionResult ObterVisaoExamePorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Exame exame = exameServico.ObterExamePorId((long)id);
+            if (exame == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exame);
+        }
+
+        private ActionResult GravarExame(Exame exame)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    exameServico.GravarExame(exame);
+                    return RedirectToAction("Index");
+                }
+                return View(exame);
+            }
+            catch
+            {
+                return View(exame);
+            }
+        }
+        // GET: Exames
+        public ActionResult Index()
+        {
+            return View(exameServico.ObterExamesClassificadosPorDesc());
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Exame exame)
+        {
+            return GravarExame(exame);
+        }
+        // GET: Edit
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoExamePorId(id);
+        }
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Exame exame)
+        {
+            return GravarExame(exame);
+        }
+
+        // GET: Delete
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoExamePorId(id);
+        }
+        // POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                Exame exame = exameServico.EliminarExamePorId(id);
+                TempData["Message"] = "Exame " + exame.Descricao.ToUpper() + " foi removido";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
