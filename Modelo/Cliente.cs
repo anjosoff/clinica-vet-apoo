dece051f6f9d176cd194b1bc958768320f55b4b0
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-12-05 22:44:24 -0300

    FEAT: Camada de Modeloo

---
+++ Modelo/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Modelo
+{
+    public class Cliente : Usuario
+    {
+        public string cpf { get; set; }
+        public IList<Pet> Pets { get; set; }
+    }
+}
---
+++ Modelo/Especie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Especie
+    {
+        public long EspecieId { get; set; }
+        public string Nome { get; set; }
+        public IList<Pet> Pets { get; set; }
+    }
+}
